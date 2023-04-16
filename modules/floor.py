8bfb3c77fcd0b3c281d9ec9655596990305ddef7
Lawliet <edwardmario123@gmail.com>
2023-04-15 20:40:25 -0500

    Añadimos el spriteMirror by orientation del player

--- modules/floor.py
+++ modules/floor.py
@@ -7,6 +7,9 @@ class Floor():
         self.level = level
         self.floors = self.randomSeed(x, y)
         self.dirtImg = pg.image.load('assets/sprites/dirtBlock.png').convert_alpha()
+        self.fireImg = pg.image.load('assets/sprites/Fire0.png').convert_alpha()
+        self.h2oImg = pg.image.load('assets/sprites/H2O.png').convert_alpha()
+        self.totemImg = pg.image.load('assets/sprites/totems1.png').convert_alpha()
 
     def randomSeed(self, x, y):
         floors = np.zeros((y, x))
@@ -40,13 +43,17 @@ class Floor():
         for i in range(len(self.floors)):
             for j in range(len(self.floors[i])):
                 if self.floors[i][j] == 1:
-                    pg.draw.rect(surface, (0,255,0), pg.Rect(j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+                    dirtImg = pg.transform.scale(self.dirtImg, (self.dirtImg.get_width() * SPRITE_SCALE, self.dirtImg.get_height() * SPRITE_SCALE))
+                    surface.blit(dirtImg, pg.Rect(j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE))
                 elif self.floors[i][j] == 2:
-                    pg.draw.rect(surface, (255,0,0), pg.Rect(j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+                    fireImg = pg.transform.scale(self.fireImg, (self.fireImg.get_width() * SPRITE_SCALE, self.fireImg.get_height() * SPRITE_SCALE))
+                    surface.blit(fireImg, pg.Rect(j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE))
                 elif self.floors[i][j] == 3:
-                    pg.draw.rect(surface, (0,0,255), pg.Rect(j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+                    h2oImg = pg.transform.scale(self.h2oImg, (self.h2oImg.get_width() * SPRITE_SCALE, self.h2oImg.get_height() * SPRITE_SCALE))
+                    surface.blit(h2oImg, pg.Rect(j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE))
                 elif self.floors[i][j] == 4:
-                    pg.draw.rect(surface, (87,35,100), pg.Rect(j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+                    totemImg = pg.transform.scale(self.totemImg, (self.totemImg.get_width() * SPRITE_SCALE, self.totemImg.get_height() * SPRITE_SCALE))
+                    surface.blit(totemImg, pg.Rect(j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
     def getFloors(self):
         return self.floors
